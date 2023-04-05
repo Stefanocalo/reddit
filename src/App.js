645ed130a51db4e5311d644d264ee79a4da2a8c3
Stefano Calo <stefano.cal92@gmail.com>
2023-04-05 18:06:11 +0200

    Add SubReddit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+//Redix imports
+import { useSelector, useDispatch} from 'react-redux';
 //Components imports
 import { NavBar } from './components/navBar/NavBar';
-import { useSelector } from 'react-redux';
+import { SubReddit } from './components/subReddit/SubReddit';
+import { useEffect } from 'react';
 
 function App() {
 
   const lightTheme = useSelector(state => state.reddit.isLightMode);
+  console.log(lightTheme)
+
+  //Fetching sunReddits
+  const dispatch = useDispatch();
+  useEffect(() => {
+    
+  },[])
 
   return (
-    <div className={lightTheme ? 'App' : 'darkApp'}>
+    <div style={{backgroundColor: lightTheme ? 'white' : 'black'}} className='App'>
       <NavBar/>
+      <SubReddit/>
     </div>
   );
 }
