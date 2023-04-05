dba93f320d1080a04157b751217ac7b7a4ad06dc
Stefano Calo <stefano.cal92@gmail.com>
2023-04-05 16:23:58 +0200

    create first NavBar structure

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 //Components imports
 import { NavBar } from './components/navBar/NavBar';
+import { useSelector } from 'react-redux';
 
 function App() {
+
+  const lightTheme = useSelector(state => state.reddit.isLightMode);
+
   return (
-    <div className="App">
+    <div className={lightTheme ? 'App' : 'darkApp'}>
       <NavBar/>
     </div>
   );
