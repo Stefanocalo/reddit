c4c6749870aaf0a9e9e8e22d459a580f981cbc8b
Stefano Calo <stefano.cal92@gmail.com>
2023-04-06 19:10:05 +0200

    Fix post images and selftext styling

--- src/components/Feed/Post.jsx
+++ src/components/Feed/Post.jsx
@@ -4,6 +4,7 @@ import { motion, AnimatePresence } from "framer-motion";
 //misc
 import { replaceString } from "../../misc/varie";
 import moment from "moment";
+import { wordShortener } from "../../misc/varie";
 //Redux imports
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAuthor } from "../../store/redditSlice";
@@ -15,6 +16,7 @@ export function Post({post, index}) {
 
     const [selectedId, setSelectedId] = useState(null);
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [expanded, setExpanded] = useState(false);
     
     const isLightMode = useSelector(state => state.reddit.isLightMode);
     
@@ -51,7 +53,23 @@ export function Post({post, index}) {
             {
                 post.selftext &&
                 <div className="selfTextContainer">
-                    <span>{post.selftext}</span>
+                    {
+                        post.selftext.length > 300 && (
+                            <>
+                            <p
+                            style={{color: isLightMode ? 'black' : 'white'}}
+                            >{wordShortener(post.selftext, expanded)}</p>
+                            <div className="expand">
+                                <span
+                                role='button'
+                                onClick={() => setExpanded(!expanded)}
+                                className="show"
+                                style={{color: isLightMode ? 'black' : 'white'}}
+                                >{expanded ? 'Show less...' : 'Show more...'}</span>
+                            </div>
+                            </>
+                        )
+                    }
                 </div>
             }
 
