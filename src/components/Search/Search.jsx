6cc88aeeb109804f042b6658f2184d457d09af38
Stefano Calo <stefano.cal92@gmail.com>
2023-04-16 15:59:31 +0200

    Add Search.tsx state and async search  fetch

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { useSelector,useDispatch } from "react-redux";
 import './Search.css';
+import { getSearchResults } from "../../api/api";
 
 export function Search() {
+
+    //Component internal status
+    const [status, setStatus] = useState({
+        isLoading: false,
+        isSuccess: false
+    });
+    //Get searchTerm from global state
+    const searchQuery = useSelector(state => state.reddit.searchTerm);
+    //Declare state variable to store search resutls
+    const [results, setResults] = useState(null);
+
+    const dispatch = useDispatch();
+    useEffect(() => {
+        try {
+            setStatus({
+                isLoading: true,
+                isSuccess: false
+            });
+            const searchResults = getSearchResults(searchQuery);
+            searchResults.then(resultList => setResults(resultList));
+        } catch (error) {
+            console.log(error);
+            //Navigate to error page.
+        }
+    },[searchQuery])
+
     return(
         <div className="searchWrapper">
-            <h1>Test</h1>
+            <h1>{searchQuery}</h1>
+            {}
         </div>
     )
 }
